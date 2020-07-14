53088b7bc15a9385c34d089b9ceb30d3c75218b4
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2020-07-15 01:10:03 +0200

    added complex derivative math, improved point finding algorithm

--- MandelMaths.js
+++ MandelMaths.js
@@ -39,16 +39,38 @@ export default class MandelMaths {
 		let dy = 0;
 		let ddx = 0;
 		let ddy = 0;
+		let dddx = 0;
+		let dddy = 0;
 		let i;
 		let points = [];
 		let steps = [];
 		for (i=0;i<maxIterations&&zx*zx+zy*zy<4;i++){
 			let d = Math.sqrt(zx*zx+zy*zy);
 			steps.push({zx,zy,d,dx,dy,i,ddx,ddy});
-			if ((zx*zx+zy*zy)/(dx*dx+dy*dy)<0.5*Math.sqrt((zx*zx+zy*zy)/((ddx*ddx+ddy*ddy)/2))){
-				let pcx = cx-(zx*dx+zy*dy)/(dx*dx+dy*dy);
-				let pcy = cy-(zy*dx-zx*dy)/(dx*dx+dy*dy);
-				points.push(new OrbitPoint(pcx,pcy,dx,dy,i+1));
+			if ((zx*zx+zy*zy)/(dx*dx+dy*dy)<Math.sqrt((zx*zx+zy*zy)/((ddx*ddx+ddy*ddy)/2))){
+				//let pcx = cx-(zx*dx+zy*dy)/(dx*dx+dy*dy);
+				//let pcy = cy-(zy*dx-zx*dy)/(dx*dx+dy*dy);
+				let roots = Complex.getQuadraticRoots(new ComplexWithDerivative(ddx/2,ddy/2,dddx/2,dddy/2),new ComplexWithDerivative(dx,dy,ddx,ddy),new ComplexWithDerivative(zx,zy,dx,dy));
+				let root = (roots[0].length<roots[1].length)?roots[0]:roots[1];
+				let pcx = cx+root.x;
+				let pcy = cy+root.y;
+				let pdx = root.dx+1;
+				let pdy = root.dy;
+				if (pdx*pdx+pdy*pdy<0.5*0.5){
+					let point = new OrbitPoint(pcx,pcy,dx,dy,i+1);
+					Object.assign(point,{pdx,pdy,ddx,ddy,dddx,dddy});
+					let discard = false;
+					for (let i2=0;i2<points.length;i2++){
+						let p = points[i2];
+						if (Complex.distance(new Complex(p.x,p.y),new Complex(point.x,point.y))<(point.orbitLength%p.orbitLength==0?0.5:0.1)*Complex.distance(new Complex(pcx,pcy),new Complex(cx,cy))){
+							discard = true;
+							break;
+						}
+					}
+					if (!discard){
+						points.push(point);
+					}
+				}
 			}
 			let zx2 = zx*zx-zy*zy+cx;
 			let zy2 = 2*zx*zy+cy;
@@ -56,12 +78,16 @@ export default class MandelMaths {
 			let dy2 = 2*(dx*zy+dy*zx);
 			let ddx2 = 2*(dx*dx-dy*dy+ddx*zx-ddy*zy);
 			let ddy2 = 2*(2*dx*dy+ddx*zy+ddy*zx);
+			let dddx2 = 6*(ddx*dx-ddy*dy)+2*(dddx*zx-dddy*zy);
+			let dddy2 = 6*(ddx*dy+ddy*dx)+2*(dddx*zy+dddy*zx);
 			zx = zx2;
 			zy = zy2;
 			dx = dx2;
 			dy = dy2;
 			ddx = ddx2;
 			ddy = ddy2;
+			dddx = dddx2;
+			dddy = dddy2;
 		}
 		points.steps = steps;
 		return points;
@@ -83,4 +109,297 @@ export class OrbitPoint {
 		this.orbitLength = orbitLength;
 	}
 }
+export class Complex {
+	
+	constructor(x=0,y=0){
+		/** @type {number} */
+		this.x = x;
+		/** @type {number} */
+		this.y = y;
+	}
+
+	scale(scale){
+		this.x *= scale;
+		this.y *= scale;
+	}
+
+	set length(length){
+		this.scale(length/this.length);
+	}
+
+	get length(){
+		return Math.sqrt(this.x*this.x+this.y*this.y);
+	}
+
+	copy(){
+		return new Complex(this.x,this.y)
+	}
+
+	/**
+	 * Multiplies this number with another.
+	 * @param {Complex|number} x
+	 * @param {number} y
+	 */
+	multiply(x,y){
+		let z = (x instanceof Complex)?x:new Complex(x,y);
+		let tx = this.x*z.x-this.y*z.y;
+		let ty = this.x*z.y+this.y*z.x;
+		this.x = tx;
+		this.y = ty;
+	}
+
+	/**
+	 * Returns one of the roots of any given complex number.
+	 * 
+	 * Always returns the one with a positive real component, or, if both have a real component of 0, the one with the larger imaginary component.
+	 * 
+	 * If the input is an object, no new Object will be created; the object will simply be modified and returned.
+	 * 
+	 * If the input is an instance of `ComplexWithDerivative`, the derivative of the result will also be calculated and updated.
+	 * @param {Complex|ComplexWithDerivative|number} x
+	 * @param {number} y
+	 * @returns {Complex|ComplexWithDerivative}
+	 */
+	static sqrt(x,y){
+		let z = (x instanceof Complex)?x:new Complex(x,y);
+		if (!(z instanceof ComplexWithDerivative)){
+			if (z.x===0&&z.y===0){
+				return z;
+			}else if (z.x>0){
+				let r = z.length;
+				z.x += r;
+				z.length = Math.sqrt(r);
+				return z;
+			}else{
+				let r = z.length;
+				z.x -= r;
+				z.length = Math.sqrt(r);
+				let temp = z.x;
+				if (z.y<0){
+					z.x = -z.y;
+					z.y = temp;
+				}else{
+					z.x = z.y;
+					z.y = -temp;
+				}
+				return z;
+			}
+		}else{
+			if (z.x===0&&z.y===0){
+				z.dx /= 0;
+				z.dy /= 0;
+				return z;
+			}else if (z.x>0){
+				let r = z.length;
+				z.x += r;
+				let d = z.derivative.copy();
+				z.length = Math.sqrt(r);
+				d.multiply(new Complex(0.5*z.x/r,-0.5*z.y/r));
+				z.derivative = d;
+				return z;
+			}else{
+				let r = z.length;
+				z.x -= r;
+				let d = z.derivative.copy();
+				z.length = Math.sqrt(r);
+				let temp = z.x;
+				if (z.y<0){
+					z.x = -z.y;
+					z.y = temp;
+				}else{
+					z.x = z.y;
+					z.y = -temp;
+				}
+				d.multiply(new Complex(0.5*z.x/r,-0.5*z.y/r));
+				z.derivative = d;
+				return z;
+			}
+		}
+	}
+
+	/**
+	 * Returns both roots (or the same one twice if there is only one) of the quadratic equation `a*z^2+b*z+c = 0`.
+	 * 
+	 * Also computes the derivatives of the roots when given the derivatives of a, b and c.
+	 * 
+	 * Original inputs remain unchanged.
+	 * @param {Complex|ComplexWithDerivative} a
+	 * @param {Complex|ComplexWithDerivative} b
+	 * @param {Complex|ComplexWithDerivative} c
+	 * @returns {Complex[]|ComplexWithDerivative[]}
+	 */
+	static getQuadraticRoots(a,b,c){
+		let ax = a.x;
+		let ay = a.y;
+		let bx = b.x;
+		let by = b.y;
+		let cx = c.x;
+		let cy = c.y;
+		if (ax===0&&ay===0){
+			if (bx===0&&by===0){
+				throw new Error("a and b can't both be both zero!");
+			}else{
+				let temp = Complex.quotient(c,b);
+				temp.scale(-1);
+				return [temp,temp.copy()];
+			}
+		}else{
+			if ((a instanceof ComplexWithDerivative)&&(b instanceof ComplexWithDerivative)&&(c instanceof ComplexWithDerivative)){
+				let dax = a.dx;
+				let day = a.dy;
+				let dbx = b.dx;
+				let dby = b.dy;
+				let dcx = c.dx;
+				let dcy = c.dy;
+				let radicantX = bx*bx-by*by-4*(ax*cx-ay*cy);
+				let radicantY = 2*bx*by-4*(ax*cy+ay*cx);
+				let radicantDx = 2*(dbx*bx-dby*by)-4*(dax*cx-day*cy+ax*dcx-ay*dcy);
+				let radicantDy = 2*(dbx*by+dby*bx)-4*(dax*cy+day*cx+ax*dcy+ay*dcx);
+				let radicantRoot = Complex.sqrt(new ComplexWithDerivative(radicantX,radicantY,radicantDx,radicantDy));
+				let divInv = Complex.inverse(new ComplexWithDerivative(2*ax,2*ay,2*dax,2*day));
+				let root1 = new ComplexWithDerivative(-bx+radicantRoot.x,-by+radicantRoot.y,-dbx+radicantRoot.dx,-dby+radicantRoot.dy);
+				let root2 = new ComplexWithDerivative(-bx-radicantRoot.x,-by-radicantRoot.y,-dbx-radicantRoot.dx,-dby-radicantRoot.dy);
+				root1.multiply(divInv);
+				root2.multiply(divInv);
+				return [root1,root2];
+			}else{
+				let radicantRoot = Complex.sqrt(bx*bx-by*by-4*(ax*cx-ay*cy),2*bx*by-4*(ax*cy+ay*cx));
+				let temp = 2*(ax*ax+ay*ay);
+				let divInv = new Complex(ax/temp,-ay/temp);
+				let root1 = new Complex(-bx+radicantRoot.x,-by+radicantRoot.y);
+				let root2 = new Complex(-bx-radicantRoot.x,-by-radicantRoot.y);
+				root1.multiply(divInv);
+				root2.multiply(divInv);
+				return [root1,root2];
+			}
+		}
+	}
+
+	/**
+	 * Returns the distance between two complex numbers.
+	 * @param {Complex} a
+	 * @param {Complex} b
+	 */
+	static distance(a,b){
+		let dx = a.x-b.x;
+		let dy = a.y-b.y;
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+
+	/**
+	 * Returns the quotient `a/b`.
+	 * 
+	 * Original inputs remain unchanged.
+	 * @param {Complex|ComplexWithDerivative} a
+	 * @param {Complex|ComplexWithDerivative} b
+	 * @returns {Complex|ComplexWithDerivative}
+	 */
+	static quotient(a,b){
+		let ax = a.x;
+		let ay = a.y;
+		let bx = b.x;
+		let by = b.y;
+		let x = (ax*bx+ay*by)/(bx*bx+by*by);
+		let y = (ay*bx-ax*by)/(bx*bx+by*by);
+		if ((a instanceof ComplexWithDerivative)&&(b instanceof ComplexWithDerivative)){
+			let dax = a.dx;
+			let day = a.dy;
+			let dbx = b.dx;
+			let dby = b.dy;
+			let tx = (dax*bx-day*by)-(ax*dbx-ay*dby);
+			let ty = (dax*by+day*bx)-(ax*dby+ay*dbx);
+			let d = bx*bx+by*by;
+			let tx2 = (bx*bx-by*by)/(d*d);
+			let ty2 = (-2*bx*by)/(d*d);
+			let dx = tx*tx2-ty*ty2;
+			let dy = tx*ty2+ty*tx2;
+			return new ComplexWithDerivative(x,y,dx,dy);
+		}else{
+			return new Complex(x,y);
+		}
+	}
+
+	/**
+	 * Sets the passed object to its multiplicative inverse `1/z` and returns it.
+	 * @param {Complex|ComplexWithDerivative} z
+	 */
+	static inverse(z){
+		let zx = z.x;
+		let zy = z.y;
+		let d = zx*zx+zy*zy;
+		let x = zx/d;
+		let y = -zy/d;
+		if (z instanceof ComplexWithDerivative){
+			let dzx = z.dx;
+			let dzy = z.dy;
+			let tx = x*x-y*y;
+			let ty = 2*x*y;
+			z.dx = -(dzx*tx-dzy*ty);
+			z.dy = -(dzx*ty+dzy*tx);
+		}
+		z.x = x;
+		z.y = y;
+		return z;
+	}
+}
+class ComplexWithDerivative extends Complex {
+	constructor(x=0,y=0,dx=0,dy=0){
+		super(x,y);
+		this.derivative = new Complex(dx,dy);
+	}
+
+	scale(scale){
+		super.scale(scale);
+		this.derivative.scale(scale);
+	}
+
+	set dx(dx){
+		this.derivative.x = dx;
+	}
+
+	get dx(){
+		return this.derivative.x;
+	}
+
+	set dy(dy){
+		this.derivative.y = dy;
+	}
+
+	get dy(){
+		return this.derivative.y;
+	}
+
+	copy(){
+		return new ComplexWithDerivative(this.x,this.y,this.dx,this.dy);
+	}
+
+	/**
+	 * Multiplies this number with another.
+	 * @param {Complex|ComplexWithDerivative|number} x
+	 * @param {number} y
+	 */
+	multiply(x,y=0){
+		let z = (x instanceof Complex)?x:new Complex(x,y);
+		let zx = z.x;
+		let zy = z.y;
+		let tx = this.x;
+		let ty = this.y;
+		let dx = this.dx;
+		let dy = this.dy;
+		this.x = tx*zx-ty*zy;
+		this.y = tx*zy+ty*zx;
+		if (z instanceof ComplexWithDerivative){
+			let dzx = z.dx;
+			let dzy = z.dy;
+			this.dx = (tx*dzx-ty*dzy)+(dx*zx-dy*zy);
+			this.dy = (tx*dzy+ty*dzx)+(dx*zy+dy*zx);
+		}else{
+			this.dx = dx*zx-dy*zy;
+			this.dy = dx*zy+dy*zx;
+		}
+	}
+}
+// exports so these can be used in the console for debugging; gonna remove these later on
 window.MandelMaths = MandelMaths;
+window.Complex = Complex;
+window.ComplexWithDerivative = ComplexWithDerivative;
