d9f80e8e1f9b091b532807d733e4cce744bd171f
Ben Eltschig <b.eltschig@protonmail.com>
2022-05-18 00:46:43 +0200

    implemented external ray tracing

--- minibrots/script.js
+++ minibrots/script.js
@@ -13,7 +13,9 @@ Utils.onPageLoad(()=>{
 		[-1.98,0,5],[-1.85,0,5],[-1.6,0,5],[-1.3,0.4,5],[-0.5,0.5,5],[-0.2,1.1,5],[0,1,5],[0.4,0.6,5],[0.4,0.3,5],
 		[-1.995,0,6],[-1.97,0,6],[-1.91,0,6],[-1.77,0,6],[-1.5,0,6],[-1.3,0.4,6],[-1.1,0.2,6],[-0.6,0.6,6],[-0.2,1.1,6],[-0.15,1.1,6],[-0.1,0.9,6],[0,1,6],[0.35,0.7,6],[0.4,0.6,6],[0.44,0.37,6],[0.4,0.2,6]
 	].map(([cx,cy,n])=>cy==0?[[cx,cy,n]]:[[cx,cy,n],[cx,-cy,n]]).flat().map(([cx,cy,n])=>formula.getNearbyCyclicPoint(cx,cy,n));*/
+	let t = Date.now();
 	const minibrots = findMinibrots(8);
+	console.log((Date.now()-t)+"ms");
 	console.log(minibrots);
 	for (let minibrot of minibrots){
 		let display = new MinibrotDisplay(minibrot);
@@ -25,14 +27,14 @@ Utils.onPageLoad(()=>{
  */
 function findMinibrots(maxPeriod=1){
 	const formula = new MandelbrotBaseFormula();
-	const minibrots = [formula.getNearbyCyclicPoint(0,0,1),formula.getNearbyCyclicPoint(-1.25,0.4,5)];
+	/*const minibrots = [formula.getNearbyCyclicPoint(0,0,1),formula.getNearbyCyclicPoint(-1.25,0.4,5)];
 	let prevMinibrots = [...minibrots];
 	for (let n=1;n<=maxPeriod;n++){
-		/** @type {(Disk|Minibrot)[]} */
+		/** @type {(Disk|Minibrot)[]} * /
 		let nextMinibrots = [];
 		for (let minibrot of prevMinibrots){
 			estimateChildren(minibrot).forEach(c=>{
-				console.log(c);
+				//console.log(c);
 				let m = formula.getNearbyCyclicPoint(c.x,c.y,minibrot.cycleLength+1);
 				if (m&&!isNaN(m.scale.length)&&!(minibrots.some(m2=>m2.equals(m))||nextMinibrots.some(m2=>m2.equals(m)))){
 					nextMinibrots.push(m);
@@ -43,9 +45,72 @@ function findMinibrots(maxPeriod=1){
 		minibrots.push(...nextMinibrots);
 		prevMinibrots = nextMinibrots;
 	}
-	//minibrots.splice(0,1);
+	//minibrots.splice(0,1);*/
+	/** @type {(Disk|Minibrot)[]} */
+	const minibrots = [];
+	for (let n=1;n<=maxPeriod;n++){
+		let m = 2**n-1;
+		middle:for (let i=0;i<m;i++){
+			for (let k=1;k<n;k++){
+				if (m%(2**k-1)==0&&(i%(m/((2**k-1))))==0){
+					// minibrot has actually minimal period k<n
+					continue middle;
+				}
+			}
+			let landingPoint = traceExternalRay(i,m);
+			let minibrot = formula.getNearbyCyclicPoint(landingPoint.x,landingPoint.y,n);
+			//console.log(`i: $${i}, $$`)
+			if (m&&!isNaN(minibrot.scale.length)&&!(minibrots.some(m2=>m2.equals(minibrot)))){
+					minibrots.push(minibrot);
+			}
+		}
+	}
 	return minibrots;
 }
+/**
+ * Traces the external ray at angle n/m.
+ * @param {number} n
+ * @param {number} m
+ */
+function traceExternalRay(n,m){
+	const bailout = 8;
+	const stepFactor = 0.4; // sort of step size, but multiplicative
+	let cx = bailout*Math.cos(n/m*Math.PI*2);
+	let cy = bailout*Math.sin(n/m*Math.PI*2);
+	for (let i=0;i<1000;i++){
+		let zx = cx;
+		let zy = cy;
+		let dzx = 1;
+		let dzy = 0;
+		let i2;
+		let k = n;
+		for (i2=0;i2<1000&&zx*zx+zy*zy<bailout*bailout;i2++){
+			let zx2 = zx*zx-zy*zy+cx;
+			let zy2 = 2*zx*zy+cy;
+			let dzx2 = 2*(dzx*zx-dzy*zy)+1.0;
+			let dzy2 = 2*(dzx*zy+dzy*zx);
+			zx = zx2;
+			zy = zy2;
+			dzx = dzx2;
+			dzy = dzy2;
+			k = (2*k)%m;
+		}
+		if (true||zx*zx+zy*zy>=bailout*bailout){
+			let sx = Math.cos(k/m*Math.PI*2);
+			let sy = Math.sin(k/m*Math.PI*2);
+			let r = Math.hypot(zx,zy);
+			let dx = stepFactor*r*sx-zx;
+			let dy = stepFactor*r*sy-zy;
+			cx += (dx*dzx+dy*dzy)/(dzx*dzx+dzy*dzy);
+			cy += (dy*dzx-dx*dzy)/(dzx*dzx+dzy*dzy);
+			//console.log(`c: ${cx.toPrecision(3)}+${cy.toPrecision(3)}i`)
+			//console.log(k,zx,zy,dzx,dzy);
+		}
+	}
+	return new Complex(cx,cy);
+}
+// @ts-ignore
+window.traceExternalRay = traceExternalRay;
 /**
  * Returns the approximate positions of two nearby minibrots of period n+1 when given one of period n.
  * @param {MandelbrotCyclicPoint} minibrot
